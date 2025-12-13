d76e08a8b44b6eacd6d890967d41fffd9b745c56
Святослав Остапенко <svyat.ostapenko@mail.ru>
2025-12-13 08:31:55 +0300

    Лабораторная работа 10

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.API_CONFIG = {
     BASE_URL: 'https://edu.std-900.ist.mospolytech.ru',
-    API_KEY: 'ВАШ_КОЛЫЧ_ЗДЕСЬ' // Замените на ваш API ключ
+    API_KEY: 'http://lab7-api.std-900.ist.mospolytech.ru/api/dishes'
 };
